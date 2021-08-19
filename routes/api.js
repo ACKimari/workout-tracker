fd92c23ae2155c63c97f3368f805de23b14356ca
A.C.Kimari <annchristine704@gmail.com>
2021-08-19 14:04:47 -0700

    added post routes

--- routes/api.js
+++ routes/api.js
@@ -9,4 +9,33 @@ router.get("/api/workouts", (req, res) => {
     .catch (err => {
         res.status(400).json(err);
     });
-})
+});
+
+router.put("/api/workouts/:id", (req, res)=> {
+    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body }}, function (err, workout) {
+        res.send(workout);
+      });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(workouts => {
+        res.json(workouts);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.post("/api/workouts/range", ({ body }, res) => {
+    Workout.insertMany(body)
+      .then(workouts => {
+        res.json(workouts);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+
+module.exports = router;
